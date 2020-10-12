2848bbf7af9861c1cd0cb6761aef44364e41f22a
jkvyff <kristianvyff@gmail.com>
2020-10-11 21:57:44 -0700

    added routes to posts by date/id

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, path: "", path_names: { sign_in: "login", sign_out: "logout", sign_up: "register" }
-  resources :posts
+  resources :posts, except: [:show]
+  get '/posts/:date/:id', to: 'posts#show'
+
   get '/about', to: 'home#about'
-  get '/home/:id', to: 'home#show'
-  get '/home', to: 'home#index'
+  get '/home/:date/:id', to: 'home#show'
+  get '/home/', to: 'home#index'
   
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
