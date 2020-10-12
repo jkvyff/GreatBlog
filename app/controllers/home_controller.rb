cc7435b6146184949b5e2f2b024b9ea0761c7023
jkvyff <kristianvyff@gmail.com>
2020-10-11 22:26:09 -0700

    added pagination to pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.page(params[:page])
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post
   include Mongoid::Document
   include Mongoid::Timestamps
+  
+  paginates_per 10
+
   field :title, type: String
   field :body, type: String
   index({title: 1}, {unique: true})
